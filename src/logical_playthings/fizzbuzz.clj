5f4d033f5a01fa62c55254d545f1c2fb73b72e65
Gergely Nagy <algernon@madhouse-project.org>
2013-02-10 23:24:01 +0100

    Convert to using conde instead of conda
    To use conde, the clauses must be disjunct. For that to happen, we
    need a not-multo, that returns true when n is a multiple of div, but
    only for numbers (so that it can be run backwards later).
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/logical_playthings/fizzbuzz.clj
+++ src/logical_playthings/fizzbuzz.clj
@@ -9,20 +9,26 @@
   (fresh [x]
          (fd/quot n div x)))
 
-(defn fizzbuzzg
+(defnc not-multo
+  [n div]
+
+  (when (number? n)
+    (not (zero? (rem n div)))))
+
+(defn fizzbuzzo
   [?n q]
 
-  (conda
-   [(multo ?n 15) (== q :fizzbuzz)]
-   [(multo ?n 3) (== q :fizz)]
-   [(multo ?n 5) (== q :buzz)]
-   [(== q ?n)]))
+  (conde
+   [(multo ?n 3) (multo ?n 5) (== q :fizzbuzz)]
+   [(multo ?n 3) (not-multo ?n 5) (== q :fizz)]
+   [(multo ?n 5) (not-multo ?n 3) (== q :buzz)]
+   [(not-multo ?n 3) (not-multo ?n 5) (== q ?n)]))
 
 (defn fizzbuzz
   [n]
 
   (first (run* [q]
-               (fizzbuzzg n q))))
+               (fizzbuzzo n q))))
 
 (defn fizzbuzz-seq
   [stop]

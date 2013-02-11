1848cee41892c0dcaf2b2d563b51f5ab9bcc4421
Gergely Nagy <algernon@madhouse-project.org>
2013-02-11 11:27:42 +0100

    Initial import
    The repository contains no code yet, just the bare bones of an
    skeleton.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,13 @@
+# logical-playthings
+
+The things herein are various, mostly independent snippets, things I
+use to learn [core.logic][clogic].
+
+## License
+
+Copyright © 2013 Gergely Nagy <algernon@madhouse-project.org>
+
+Distributed under the [GNU LGPL][lgpl] v3+.
+
+ [clogic]: https://github.com/clojure/core.logic
+ [lgpl]: http://www.gnu.org/licenses/lgpl.html
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject logical-playthings "0.0.0-SNAPSHOT"
+  :description "core.logic playthings"
+  :url "https://github.com/algernon/logical-playthings"
+  :license {:name "GNU Lesser General Public License v3+"
+            :url "http://www.gnu.org/licenses/lgpl.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.8.3-rc3-SNAPSHOT"]])

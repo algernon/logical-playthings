0f7a7725e42989ed8e7c4f7eb589a43d2043111a
Gergely Nagy <algernon@madhouse-project.org>
2013-02-11 11:43:09 +0100

    fizzbuzz: Extract tests into a separate file
    Now that it is not contained in a gist, within one single file, split
    the tests out into a separate file.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/logical_playthings/fizzbuzz.clj
+++ src/logical_playthings/fizzbuzz.clj
@@ -1,7 +1,7 @@
-(refer 'clojure.core :exclude [==])
-(use '[clojure.core.logic])
-(require '[clojure.core.logic.fd :as fd])
-(require '[clojure.test :as t])
+(ns logical-playthings.fizzbuzz
+  (:refer-clojure :exclude [==])
+  (:use [clojure.core.logic])
+  (:require [clojure.core.logic.fd :as fd]))
 
 (defn multo
   "Ensure that `n` is a multiplicate of `div`."
@@ -48,55 +48,3 @@
   (run* [q]
         (fizzbuzzo q v)
         (fd/in q (fd/interval 1 max))))
-
-;; --| Tests |--
-
-(t/deftest test:fizzbuzz
-  (t/testing "The fizzbuzz function"
-    (t/testing "with numbers returning themselves"
-      (t/is (= (fizzbuzz 1) 1))
-      (t/is (= (fizzbuzz 2) 2))
-      (t/is (= (fizzbuzz 4) 4))
-      (t/is (= (fizzbuzz 98) 98)))
-    (t/testing "with numbers returning :fizz"
-      (t/is (= (fizzbuzz 3) :fizz))
-      (t/is (= (fizzbuzz 6) :fizz)))
-    (t/testing "with numbers returning :buzz"
-      (t/is (= (fizzbuzz 5) :buzz))
-      (t/is (= (fizzbuzz 10) :buzz)))
-    (t/testing "with numbers returning :fizzbuzz"
-      (t/is (= (fizzbuzz 15) :fizzbuzz))
-      (t/is (= (fizzbuzz 30) :fizzbuzz)))))
-
-(t/deftest test:fizzbuzz-seq
-  (t/testing "The fizzbuzz-seq function"
-    (t/is (= (fizzbuzz-seq 15)
-             [1 2 :fizz 4 :buzz :fizz 7 8 :fizz :buzz
-              11 :fizz 13 14 :fizzbuzz]))))
-
-(t/deftest test:fizzbuzz-backwards
-  (t/testing "Running fizzbuzz backwards"
-    (t/testing "and finding numbers that return 1"
-      (t/is (= (find-fizzbuzz-index-for 1 10)
-               [1])))
-    (t/testing "and finding numbers that return :fizz"
-      (t/is (= (find-fizzbuzz-index-for :fizz 30)
-               [3 6 9 12 18 21 24 27])))
-    (t/testing "and finding numbers that return :buzz"
-      (t/is (= (find-fizzbuzz-index-for :buzz 30)
-               [5 10 20 25])))
-    (t/testing "and finding numbers that return :fizzbuzz"
-      (t/is (= (find-fizzbuzz-index-for :fizzbuzz 30)
-               [15 30]))))
-
-  (t/testing "Running fizzbuzz back and forth"
-    (t/is (= (map fizzbuzz (find-fizzbuzz-index-for :fizz 30))
-             (filter #{:fizz} (fizzbuzz-seq 30))))
-    (t/is (= (map fizzbuzz (find-fizzbuzz-index-for :buzz 30))
-             (filter #{:buzz} (fizzbuzz-seq 30))))
-    (t/is (= (map fizzbuzz (find-fizzbuzz-index-for :fizzbuzz 30))
-             (filter #{:fizzbuzz} (fizzbuzz-seq 30))))))
-
-(comment
-  (t/run-tests)
-)
---
+++ test/logical_playthings/fizzbuzz_test.clj
@@ -0,0 +1,49 @@
+(ns logical-playthings.fizzbuzz-test
+  (:use [clojure.test]
+        [logical-playthings.fizzbuzz]))
+
+(deftest test:fizzbuzz
+  (testing "The fizzbuzz function"
+    (testing "with numbers returning themselves"
+      (is (= (fizzbuzz 1) 1))
+      (is (= (fizzbuzz 2) 2))
+      (is (= (fizzbuzz 4) 4))
+      (is (= (fizzbuzz 98) 98)))
+    (testing "with numbers returning :fizz"
+      (is (= (fizzbuzz 3) :fizz))
+      (is (= (fizzbuzz 6) :fizz)))
+    (testing "with numbers returning :buzz"
+      (is (= (fizzbuzz 5) :buzz))
+      (is (= (fizzbuzz 10) :buzz)))
+    (testing "with numbers returning :fizzbuzz"
+      (is (= (fizzbuzz 15) :fizzbuzz))
+      (is (= (fizzbuzz 30) :fizzbuzz)))))
+
+(deftest test:fizzbuzz-seq
+  (testing "The fizzbuzz-seq function"
+    (is (= (fizzbuzz-seq 15)
+             [1 2 :fizz 4 :buzz :fizz 7 8 :fizz :buzz
+              11 :fizz 13 14 :fizzbuzz]))))
+
+(deftest test:fizzbuzz-backwards
+  (testing "Running fizzbuzz backwards"
+    (testing "and finding numbers that return 1"
+      (is (= (find-fizzbuzz-index-for 1 10)
+               [1])))
+    (testing "and finding numbers that return :fizz"
+      (is (= (find-fizzbuzz-index-for :fizz 30)
+               [3 6 9 12 18 21 24 27])))
+    (testing "and finding numbers that return :buzz"
+      (is (= (find-fizzbuzz-index-for :buzz 30)
+               [5 10 20 25])))
+    (testing "and finding numbers that return :fizzbuzz"
+      (is (= (find-fizzbuzz-index-for :fizzbuzz 30)
+               [15 30]))))
+
+  (testing "Running fizzbuzz back and forth"
+    (is (= (map fizzbuzz (find-fizzbuzz-index-for :fizz 30))
+             (filter #{:fizz} (fizzbuzz-seq 30))))
+    (is (= (map fizzbuzz (find-fizzbuzz-index-for :buzz 30))
+             (filter #{:buzz} (fizzbuzz-seq 30))))
+    (is (= (map fizzbuzz (find-fizzbuzz-index-for :fizzbuzz 30))
+             (filter #{:fizzbuzz} (fizzbuzz-seq 30))))))

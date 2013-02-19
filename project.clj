eba04cf7ea97615c106ad9b3ef09fe443484742f
Gergely Nagy <algernon@madhouse-project.org>
2013-02-19 10:46:26 +0100

    project.clj: Add the sonatype repos, for core.logic snapshots
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "GNU Lesser General Public License v3+"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.8.0-rc3-SNAPSHOT"]])
+                 [org.clojure/core.logic "0.8.0-rc3-SNAPSHOT"]]
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
